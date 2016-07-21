e267c325127201461406ac430d0f538740a1b778
Colin McDonald <mickydwow@gmail.com>
2016-07-21 12:08:51 -0700

    Food-chain

---
+++ food-chain/food-chain.js
@@ -0,0 +1,11 @@
+var FoodChain = function() {};
+
+FoodChain.prototype.verse = function(num) {
+  if (num === 1) {
+    return 'I know an old lady who swallowed a fly.\nI don\'t know why she swallowed the fly. Perhaps she\'ll die.\n'
+  }
+
+  
+};
+
+module.exports = FoodChain;
